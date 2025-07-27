1af924108a00060b2897c907a86bba510b816520
Ron Kow <ronkow@gmail.com>
2025-07-27 03:49:38 -0700

    Create lesson0.py

---
+++ lesson0.py
@@ -0,0 +1,38 @@
+# LESSON 0: INTRODUCTION
+
+'''
+Welcome!
+This course in Python programming is for people who have never written and run any code in any programming language before.
+It assumes you have some basic math knowledge (e.g. prime numbers) and you understand basic computing terminology (e.g. file extensions).   
+However, it does not assume you know any programming terminology and concepts.
+To be a  general programming terminology, concepts and Python best practices. 
+
+Your goal is to understand every line of code in the examples.
+After that, type (or modify, if you like) and run the code to build your muscle memory.
+
+Before we begin Lesson 1, let's discuss the syntax for COMMENTS.
+If you write code 
+It's always good to put more comments in your code. 
+When you run your code, Python ignores all comments.
+
+All explanations in our lessons are written as comments.
+Programming terminology are written in UPPERCASE.
+
+You can type comments in two ways: using # or using triple quotes.
+'''
+
+# Syntax 1: Single-line comment. This is a single-line comment.
+
+'''
+Syntax 2: Multi-line comment
+This is a line comment.
+This is another line of comment.
+'''
+
+"""
+Syntax 2: Multi-line comment
+You can also use double quotes. Python reads single and double quotes the same way.
+I always use single quotes. Personal preference.
+"""
+
+# END OF LESSON
