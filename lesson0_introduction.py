be399094172654c18ea438b175e8dac1fb240061
Ron Kow <ronkow@gmail.com>
2025-07-27 04:31:27 -0700

    Update lesson0_introduction.py

--- lesson0_introduction.py
+++ lesson0_introduction.py
@@ -1,15 +1,6 @@
 # LESSON 0: INTRODUCTION
 
 '''
-Welcome!
-This course in Python programming is for people who have never written and run any code in any programming language before.
-It assumes you have some basic math knowledge (e.g. prime numbers) and you understand basic computing terminology (e.g. file extensions).   
-However, it does not assume you know any programming terminology and concepts.
-To be a  general programming terminology, concepts and Python best practices. 
-
-Your goal is to understand every line of code in the examples.
-After that, type (or modify, if you like) and run the code to build your muscle memory.
-
 Before we begin Lesson 1, let's discuss the syntax for COMMENTS.
 If you write code 
 It's always good to put more comments in your code. 
